3a66ffcaeca00455ce9711e7cfec401d49b1c010
Adam Burucs <adam.burucs@insdata.local>
2017-12-21 12:09:55 +0100

    Add explain text

--- readability-test/readability.js
+++ readability-test/readability.js
@@ -60,15 +60,46 @@ function styleResult(result, score) {
   }
 }
 
+function resultExplained(resultExp, score) {
+  let explain = '';
+  const explainTexts = [
+    'Very easy to read.',
+    'Easy to read.',
+    'Fairly easy to read.',
+    'Plain English.',
+    'Fairly difficult to read.',
+    'Difficult to read.',
+    'Very difficult to read.'
+  ];
+  if (score < 30) {
+    explain = explainTexts[6];
+  } else if (score >= 30 && score < 50) {
+    explain = explainTexts[5];
+  } else if (score >= 50 && score < 60) {
+    explain = explainTexts[4];
+  } else if (score >= 60 && score < 70) {
+    explain = explainTexts[3];
+  } else if (score >= 70 && score < 80) {
+    explain = explainTexts[2];
+  } else if (score >= 80 && score < 90) {
+    explain = explainTexts[1];
+  } else if (score >= 90) {
+    explain = explainTexts[0];
+  }
+  resultExp.innerHTML = explain;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelector("#score-button").addEventListener("click", function () {
-    const mainText = document.querySelector("#main-text").value;
-    const language = document.querySelector("#language").value;
-    const result   = document.querySelector("#result");
+    const mainText  = document.querySelector("#main-text").value;
+    const language  = document.querySelector("#language").value;
+    const result    = document.querySelector("#result");
+    const resultExp = document.querySelector("#result-explained");
     let run = new Readability(mainText, language);
     run.calcScore();
     let score = run.score;
     result.innerHTML = score;
     styleResult(result, score);
+    resultExplained(resultExp, score);
   });
 });

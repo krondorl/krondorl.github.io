45c29caa19c3377ddf3c4a6c5567d22090e118c4
Adam Burucs <adam.burucs@insdata.local>
2017-12-21 11:01:00 +0100

    Fix zero score bug

--- readability-test/readability.js
+++ readability-test/readability.js
@@ -14,36 +14,20 @@ class Readability {
 
   calcSentences() {
     let sentences = this.text;
-    let count = sentences.replace(/\w[.?!](\s|$)/g, "$1|").split("|");
+    let count = sentences.replace(/\w[.?!](\s|$)/g, "$1|").split("|").length;
     this.sentences = count - 1;
   }
 
-  calcHunSyllables(word) {
-    word = word.toLowerCase();
-    return word.match(/[aáuúüűoóöőiíeé]/g).length;
-  }
-
   calcSyllablesHun() {
-    let words = this.text.split(' ');
-    let syllables = words.reduce(
-      ( acc, cur ) => acc + this.calcHunSyllables(cur), 0
-    );
-    this.syllables = syllables;
-  }
-
-  calcSyllablesEng(word) {
-    word = word.toLowerCase();
-    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-    word = word.replace(/^y/, '');
-    return word.match(/[aeiouy]{1,2}/g).length;
+    let text = this.text.toLowerCase();
+    this.syllables = text.match(/[aáuúüűoóöőiíeé]/g).length;
   }
 
   calcSyllablesEng() {
-    let words = this.text.split(' ');
-    let syllables = words.reduce(
-      ( acc, cur ) => acc + calcSyllablesEng(cur), 0
-    );
-    this.syllables = syllables;
+    let word = this.text.toLowerCase();
+    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+    word = word.replace(/^y/, '');
+    this.syllables = word.match(/[aeiouy]{1,2}/g).length;
   }
 
   calcScore() {
@@ -61,6 +45,7 @@ class Readability {
       this.score = 206.835
         - 1.015 * (this.words / this.sentences)
         - 84.6 * (this.syllables / this.words);
+      this.score = parseInt(this.score);
     }
   }
 }

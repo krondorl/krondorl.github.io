a6c41a9a87736ea30c76930dc3001c2dafe30eac
Adam Burucs <adam.burucs@insdata.local>
2018-01-15 15:17:33 +0100

    Update Pizza Kalk

--- pizza-kalkulator/pizza.js
+++ pizza-kalkulator/pizza.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // Diameter in cm
+  calcPizza = (price, diameter) => {
+    return (price / ((Math.pow((diameter), 2) * Math.PI) / 4));
+  }
+
   document.querySelector(".button-reset").addEventListener("click", function () {
     let inputs = Array.from(document.querySelectorAll("input[type=text]"));
     for (input of inputs) {
@@ -7,7 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
   });
   document.querySelector(".button-calculate").addEventListener("click", function () {
     let inputs = Array.from(document.querySelectorAll("input[type=text]"));
+    let data   = [];
     for (input of inputs) {
+      let rawName      = input.name;
+      let name         = rawName.split('--');
+      let key          = name[0];
+      let value        = input.value;
+      let index        = name[1];
+      data.push({ index, key, value });
     }
+    console.log(data);
   });
 });
